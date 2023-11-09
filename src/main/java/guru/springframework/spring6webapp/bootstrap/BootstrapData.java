0c750ca61a557a3df25a63e160211f39a2ab48e8
Faintful <72162133+Faintful@users.noreply.github.com>
2023-11-09 13:29:54 +0100

    My solution to Assignment 1 - Add Publisher Entity

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,26 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
 //      --- Instantiating new entities ---
+
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("name");
+        publisher.setAddress("address");
+        publisher.setState("state");
+        publisher.setCity("city");
+        publisher.setZip("zip");
+
 
         Author eric = new Author();
         eric.setFirstName("Eric");
@@ -40,6 +52,8 @@ public class BootstrapData implements CommandLineRunner {
         noEJB.setIsbn("54757585");
 
 //      --- Persists the newly created instance to the database and saves the return value to a variable for further operations ---
+
+        publisherRepository.save(publisher); // Because the exercise does not ask for further operations, saving to a local variable is not necessary
 
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
@@ -62,5 +76,6 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
